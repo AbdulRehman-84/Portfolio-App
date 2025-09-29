0fd6fbb87155d768b0196c46052958208bfbb112
AbdulRehman-84 <home7837107@gmail.com>
2025-09-29 11:29:02 +0500

    Portfolio

---
+++ lib/views/about_me.dart
@@ -0,0 +1,107 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'package:my_portfolio/globals/app_assets.dart';
+import 'package:my_portfolio/globals/app_colors.dart';
+import 'package:my_portfolio/globals/constants.dart';
+import 'package:my_portfolio/helper%20class/helper_class.dart';
+import '../globals/app_buttons.dart';
+import '../globals/app_text_styles.dart';
+
+class AboutMe extends StatelessWidget {
+  const AboutMe({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return HelperClass(
+      mobile: Column(
+        children: [
+          buildAboutMeContents(),
+          Constants.sizedBox(height: 35.0),
+          buildProfilePicture(),
+        ],
+      ),
+      tablet: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          buildProfilePicture(),
+          Constants.sizedBox(width: 25.0),
+          Expanded(child: buildAboutMeContents())
+        ],
+      ),
+      desktop: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          buildProfilePicture(),
+          Constants.sizedBox(width: 25.0),
+          Expanded(child: buildAboutMeContents())
+        ],
+      ),
+      paddingWidth: size.width * 0.1,
+      bgColor: AppColors.bgColor2,
+    );
+  }
+
+  FadeInRight buildProfilePicture() {
+    return FadeInRight(
+      duration: const Duration(milliseconds: 1200),
+      child: Image.asset(
+        AppAssets.profile2,
+        height: 450,
+        width: 400,
+      ),
+    );
+  }
+
+  Column buildAboutMeContents() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        FadeInRight(
+          duration: const Duration(milliseconds: 1200),
+          child: RichText(
+            text: TextSpan(
+              text: 'About ',
+              style: AppTextStyles.headingStyles(fontSize: 30.0),
+              children: [
+                TextSpan(
+                  text: 'Me!',
+                  style: AppTextStyles.headingStyles(
+                      fontSize: 30, color: AppColors.robinEdgeBlue),
+                )
+              ],
+            ),
+          ),
+        ),
+        Constants.sizedBox(height: 6.0),
+        FadeInLeft(
+          duration: const Duration(milliseconds: 1400),
+          child: Text(
+            'Flutter Developer!',
+            style: AppTextStyles.montserratStyle(color: Colors.white),
+          ),
+        ),
+        Constants.sizedBox(height: 8.0),
+        FadeInLeft(
+          duration: const Duration(milliseconds: 1600),
+          child: Text(
+            'I am a passionate Flutter Developer with a strong focus on building beautiful, responsive, and high-performance mobile applications. '
+            'With expertise in UI/UX design, state management, and Firebase integration, I strive to create seamless user experiences.'
+            'I enjoy solving complex problems and developing scalable solutions that enhance app functionality and usability.'
+            'Always eager to learn and experiment with new technologies, I am committed to delivering quality-driven applications.'
+            'I am open to collaborations and exciting projects, '
+            'where I can contribute my skills to create impactful digital solutions.',
+            style: AppTextStyles.normalStyle(),
+          ),
+        ),
+        Constants.sizedBox(height: 15.0),
+        FadeInUp(
+          duration: const Duration(milliseconds: 1800),
+          child: AppButtons.buildMaterialButton(
+              onTap: () {}, buttonName: 'Read More'),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/views/home_page.dart
@@ -0,0 +1,210 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:my_portfolio/globals/app_assets.dart';
+import 'package:my_portfolio/globals/app_buttons.dart';
+import 'package:my_portfolio/globals/app_colors.dart';
+import 'package:my_portfolio/globals/app_text_styles.dart';
+import 'package:my_portfolio/globals/constants.dart';
+import 'package:my_portfolio/helper%20class/helper_class.dart';
+import 'package:my_portfolio/widgets/profile_animation.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final socialButtons = <String>[
+    AppAssets.facebook,
+    AppAssets.twitter,
+    AppAssets.linkedIn,
+    AppAssets.insta,
+    AppAssets.github,
+  ];
+  final socialLinks = <String>[
+    "https://facebook.com/yourprofile",
+    "https://twitter.com/yourprofile",
+    "https://www.linkedin.com/in/abdul-rehman-a19787261/",
+    "https://instagram.com/yourprofile",
+    "https://github.com/AbdulRehman-84",
+  ];
+
+  var socialBI;
+  // url open karne ka function
+  Future<void> _launchURL(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri, mode: LaunchMode.externalApplication);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return HelperClass(
+      mobile: Column(
+        children: [
+          buildHomePersonalInfo(size),
+          Constants.sizedBox(height: 25.0),
+          const ProfileAnimation()
+        ],
+      ),
+      tablet: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Expanded(child: buildHomePersonalInfo(size)),
+          const ProfileAnimation(),
+        ],
+      ),
+      desktop: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Expanded(child: buildHomePersonalInfo(size)),
+          const ProfileAnimation(),
+        ],
+      ),
+      paddingWidth: size.width * 0.1,
+      bgColor: Colors.transparent,
+    );
+  }
+
+  Column buildHomePersonalInfo(Size size) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        FadeInDown(
+          duration: const Duration(milliseconds: 1200),
+          child: Text(
+            'Hello, It\'s Me',
+            style: AppTextStyles.montserratStyle(color: Colors.white),
+          ),
+        ),
+        Constants.sizedBox(height: 15.0),
+        FadeInRight(
+          duration: const Duration(milliseconds: 1400),
+          child: Text(
+            'Abdul Rehman',
+            style: AppTextStyles.headingStyles(),
+          ),
+        ),
+        Constants.sizedBox(height: 15.0),
+        FadeInLeft(
+          duration: const Duration(milliseconds: 1400),
+          child: Row(
+            children: [
+              Text(
+                'And I\'m a ',
+                style: AppTextStyles.montserratStyle(color: Colors.white),
+              ),
+              AnimatedTextKit(
+                animatedTexts: [
+                  TyperAnimatedText(
+                    'Flutter Developer',
+                    textStyle:
+                        AppTextStyles.montserratStyle(color: Colors.lightBlue),
+                  ),
+                  TyperAnimatedText('Flutter',
+                      textStyle: AppTextStyles.montserratStyle(
+                          color: Colors.lightBlue)),
+                  TyperAnimatedText('Developer',
+                      textStyle: AppTextStyles.montserratStyle(
+                          color: Colors.lightBlue))
+                ],
+                pause: const Duration(milliseconds: 1000),
+                displayFullTextOnTap: true,
+                stopPauseOnTap: true,
+              )
+            ],
+          ),
+        ),
+        Constants.sizedBox(height: 15.0),
+        FadeInDown(
+          duration: const Duration(milliseconds: 1600),
+          child: Expanded(
+            child: Text(
+              '👨‍💻 Flutter Developer | Crafting beautiful & responsive apps 🚀'
+              '📱 Expertise in UI/UX, state management, and Firebase integration 🔥'
+              '🌍 Open to collaboration & exciting Flutter projects! ✨',
+              style: AppTextStyles.normalStyle(),
+            ),
+          ),
+        ),
+        Constants.sizedBox(height: 22.0),
+        FadeInUp(
+          duration: const Duration(milliseconds: 1600),
+          child: SizedBox(
+            height: 48,
+            child: ListView.separated(
+              itemCount: socialButtons.length,
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              separatorBuilder: (context, child) =>
+                  Constants.sizedBox(width: 8.0),
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {
+                    _launchURL(socialLinks[index]);
+                  },
+                  onHover: (value) {
+                    setState(() {
+                      if (value) {
+                        socialBI = index;
+                      } else {
+                        socialBI = null;
+                      }
+                    });
+                  },
+                  borderRadius: BorderRadius.circular(550.0),
+                  hoverColor: AppColors.themeColor,
+                  splashColor: AppColors.lawGreen,
+                  child: buildSocialButton(
+                      asset: socialButtons[index],
+                      hover: socialBI == index ? true : false),
+                );
+              },
+            ),
+          ),
+        ),
+        Constants.sizedBox(height: 18.0),
+        FadeInUp(
+          duration: const Duration(milliseconds: 1800),
+          child: AppButtons.buildMaterialButton(
+              onTap: () {
+                _launchURL(
+                    "https://raw.githubusercontent.com/AbdulRehman-84/Portfolio-CV/main/Abdul%20Rehman%20Resume.pdf");
+              },
+              buttonName: 'Download CV'),
+        ),
+      ],
+    );
+  }
+
+  Ink buildSocialButton({required String asset, required bool hover}) {
+    return Ink(
+      width: 45,
+      height: 45,
+      decoration: BoxDecoration(
+        border: Border.all(color: AppColors.themeColor, width: 2.0),
+        color: AppColors.bgColor,
+        shape: BoxShape.circle,
+      ),
+      padding: const EdgeInsets.all(6),
+      child: Image.asset(
+        asset,
+        width: 10,
+        height: 12,
+        color: hover ? AppColors.bgColor : AppColors.themeColor,
+        // fit: BoxFit.fill,
+      ),
+    );
+  }
+}
